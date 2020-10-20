6898dee2e4d932fa78b42b300111f5b83687bfe6
gilbert <gilbertgozalie@gmail.com>
2020-10-20 16:39:09 +0700

    Initial Commit

---
+++ CustomedRoundView/CustomedRoundView.h
@@ -0,0 +1,18 @@
+//
+//  CustomedRoundView.h
+//  CustomedRoundView
+//
+//  Created by Gilbert Gozalie on 10/20/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomedRoundView.
+FOUNDATION_EXPORT double CustomedRoundViewVersionNumber;
+
+//! Project version string for CustomedRoundView.
+FOUNDATION_EXPORT const unsigned char CustomedRoundViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomedRoundView/PublicHeader.h>
+
+
